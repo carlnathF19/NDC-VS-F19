c5241f0016da78a08d20e6d08fc71d13edc8c3fe
carlnathF19 <54322367+carlnathF19@users.noreply.github.com>
2019-11-04 10:49:31 -0700

    7-1 update
    updated listbox, now clears and adds each stock at the top of it's respective grouping.

---
+++ assignments/ASG7-1/ASG7-1/Stock Market.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MainMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CalculateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SummaryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tuesdayValMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.mondayValMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.wednesdayValMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.stockCodeTextBox = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CalculateToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SummaryToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.stockListBox = New System.Windows.Forms.ListBox()
+        Me.MainMenuStrip.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MainMenuStrip
+        '
+        Me.MainMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MainMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MainMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MainMenuStrip.Name = "MainMenuStrip"
+        Me.MainMenuStrip.Size = New System.Drawing.Size(810, 28)
+        Me.MainMenuStrip.TabIndex = 2
+        Me.MainMenuStrip.Text = "MenuStrip"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(116, 26)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CalculateToolStripMenuItem, Me.ClearToolStripMenuItem, Me.SummaryToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'CalculateToolStripMenuItem
+        '
+        Me.CalculateToolStripMenuItem.Name = "CalculateToolStripMenuItem"
+        Me.CalculateToolStripMenuItem.Size = New System.Drawing.Size(154, 26)
+        Me.CalculateToolStripMenuItem.Text = "&Calculate"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(154, 26)
+        Me.ClearToolStripMenuItem.Text = "C&lear"
+        '
+        'SummaryToolStripMenuItem
+        '
+        Me.SummaryToolStripMenuItem.Name = "SummaryToolStripMenuItem"
+        Me.SummaryToolStripMenuItem.Size = New System.Drawing.Size(154, 26)
+        Me.SummaryToolStripMenuItem.Text = "&Summary"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(55, 24)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(133, 26)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'tuesdayValMaskedTextBox
+        '
+        Me.tuesdayValMaskedTextBox.Location = New System.Drawing.Point(168, 50)
+        Me.tuesdayValMaskedTextBox.Name = "tuesdayValMaskedTextBox"
+        Me.tuesdayValMaskedTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.tuesdayValMaskedTextBox.TabIndex = 3
+        Me.tuesdayValMaskedTextBox.Text = "2"
+        Me.ToolTip1.SetToolTip(Me.tuesdayValMaskedTextBox, "Please enter Tuesday's stock price with no $ sign.")
+        '
+        'mondayValMaskedTextBox
+        '
+        Me.mondayValMaskedTextBox.Location = New System.Drawing.Point(38, 50)
+        Me.mondayValMaskedTextBox.Name = "mondayValMaskedTextBox"
+        Me.mondayValMaskedTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.mondayValMaskedTextBox.TabIndex = 2
+        Me.mondayValMaskedTextBox.Text = "1"
+        Me.ToolTip1.SetToolTip(Me.mondayValMaskedTextBox, "Please enter Monday's stock price with no $ sign.")
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Monday"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(165, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Tuesday"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(293, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Wednesday"
+        '
+        'wednesdayValMaskedTextBox
+        '
+        Me.wednesdayValMaskedTextBox.Location = New System.Drawing.Point(296, 50)
+        Me.wednesdayValMaskedTextBox.Name = "wednesdayValMaskedTextBox"
+        Me.wednesdayValMaskedTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.wednesdayValMaskedTextBox.TabIndex = 4
+        Me.wednesdayValMaskedTextBox.Text = "5"
+        Me.ToolTip1.SetToolTip(Me.wednesdayValMaskedTextBox, "Please enter Wednesday's stock price with no $ sign.")
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 53)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(16, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "$"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(146, 53)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(16, 17)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "$"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(274, 50)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(16, 17)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "$"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.tuesdayValMaskedTextBox)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.mondayValMaskedTextBox)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.wednesdayValMaskedTextBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(368, 31)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(420, 95)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Prices"
+        '
+        'stockCodeTextBox
+        '
+        Me.stockCodeTextBox.Location = New System.Drawing.Point(8, 50)
+        Me.stockCodeTextBox.Name = "stockCodeTextBox"
+        Me.stockCodeTextBox.Size = New System.Drawing.Size(172, 22)
+        Me.stockCodeTextBox.TabIndex = 1
+        Me.stockCodeTextBox.Text = "APP"
+        Me.ToolTip1.SetToolTip(Me.stockCodeTextBox, "Enter the stock code here.")
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.stockCodeTextBox)
+        Me.GroupBox2.Location = New System.Drawing.Point(176, 31)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(186, 95)
+        Me.GroupBox2.TabIndex = 15
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "NYSE Stock Code"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(181, 129)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(504, 51)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Please enter the Stock Code with it's Monday, Tuesday, and Wednesday price." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Calc" &
+    "ulate, Clear, and Summary are located in the Edit menu" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " "
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CalculateToolStripMenuItem1, Me.ClearToolStripMenuItem1, Me.SummaryToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(141, 76)
+        '
+        'CalculateToolStripMenuItem1
+        '
+        Me.CalculateToolStripMenuItem1.Name = "CalculateToolStripMenuItem1"
+        Me.CalculateToolStripMenuItem1.Size = New System.Drawing.Size(140, 24)
+        Me.CalculateToolStripMenuItem1.Text = "&Calculate"
+        '
+        'ClearToolStripMenuItem1
+        '
+        Me.ClearToolStripMenuItem1.Name = "ClearToolStripMenuItem1"
+        Me.ClearToolStripMenuItem1.Size = New System.Drawing.Size(140, 24)
+        Me.ClearToolStripMenuItem1.Text = "C&lear"
+        '
+        'SummaryToolStripMenuItem1
+        '
+        Me.SummaryToolStripMenuItem1.Name = "SummaryToolStripMenuItem1"
+        Me.SummaryToolStripMenuItem1.Size = New System.Drawing.Size(140, 24)
+        Me.SummaryToolStripMenuItem1.Text = "&Summary"
+        '
+        'stockListBox
+        '
+        Me.stockListBox.FormattingEnabled = True
+        Me.stockListBox.ItemHeight = 16
+        Me.stockListBox.Location = New System.Drawing.Point(184, 183)
+        Me.stockListBox.Name = "stockListBox"
+        Me.stockListBox.Size = New System.Drawing.Size(604, 308)
+        Me.stockListBox.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(810, 528)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.stockListBox)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MainMenuStrip)
+        Me.Name = "Form1"
+        Me.Text = "Stock Market Calculator"
+        Me.MainMenuStrip.ResumeLayout(False)
+        Me.MainMenuStrip.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MainMenuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CalculateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SummaryToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents tuesdayValMaskedTextBox As MaskedTextBox
+    Friend WithEvents mondayValMaskedTextBox As MaskedTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents wednesdayValMaskedTextBox As MaskedTextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents stockCodeTextBox As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents CalculateToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SummaryToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents stockListBox As ListBox
+End Class
---
+++ assignments/ASG7-1/ASG7-1/Stock Market.vb
@@ -0,0 +1,140 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+    Dim summary As New List(Of String)
+    Dim endSummary As New List(Of String)
+
+    'Private Function HorizontalBorder(length As Integer, characterSet As String) As String
+    '    Dim tempstring As String = ""
+    '    For i = 0 To length
+    '        tempstring &= characterSet
+    '    Next
+
+    '    Return tempstring
+    'End Function
+    Private Function ColumnHeaders(firstColumn As String, secondColumn As String, thirdColumn As String, fourthColumn As String) As String
+        'Description, Unit, Rate, Subtotal
+        Dim columnWidth As Integer = 20
+        firstColumn = "| " & firstColumn
+        secondColumn = "| " & secondColumn
+        thirdColumn = "| " & thirdColumn
+        fourthColumn = "| " & fourthColumn
+
+        Return firstColumn.PadRight(columnWidth) & secondColumn.PadRight(columnWidth) &
+         thirdColumn.PadRight(columnWidth) & fourthColumn.PadRight(columnWidth - 1) & "|"
+
+    End Function
+    Private Sub clearData()
+        ' []update to clear summary?
+        stockCodeTextBox.Clear()
+        mondayValMaskedTextBox.Clear()
+        tuesdayValMaskedTextBox.Clear()
+        wednesdayValMaskedTextBox.Clear()
+        stockListBox.Items.Clear()
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CalculateToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CalculateToolStripMenuItem.Click,
+        CalculateToolStripMenuItem1.Click
+        'Validates code and returns messagebox with any fields that are invalid, selecting the first in the tab order.
+        Dim errorSummary As String = ""
+        Dim firstDayChange As Double
+        Dim secondDayChange As Double
+        Dim decision As String
+        Static buyCount As Integer = 0
+        Static sellCount As Integer = 0
+        Static holdCount As Integer = 0
+
+
+        Try
+            Double.Parse(wednesdayValMaskedTextBox.Text)
+        Catch ex As Exception
+            errorSummary = "Please enter a valid Price in Wednesday." & vbNewLine & errorSummary
+            wednesdayValMaskedTextBox.Select()
+        End Try
+        Try
+            Double.Parse(tuesdayValMaskedTextBox.Text)
+        Catch ex As Exception
+            errorSummary = "Please enter a valid Price in Tuesday." & vbNewLine & errorSummary
+            tuesdayValMaskedTextBox.Select()
+        End Try
+        Try
+            Double.Parse(mondayValMaskedTextBox.Text)
+        Catch ex As Exception
+            errorSummary = "Please enter a valid Price in Monday." & vbNewLine & errorSummary
+            mondayValMaskedTextBox.Select()
+        End Try
+        If stockCodeTextBox.Text = "" Then
+            errorSummary = "Please enter the stock code you would like to calculate." _
+                & vbNewLine & errorSummary
+            stockCodeTextBox.Select()
+        End If
+
+        If errorSummary <> "" Then
+            MessageBox.Show(errorSummary)
+        End If
+
+        If errorSummary = "" Then
+            firstDayChange = Double.Parse(tuesdayValMaskedTextBox.Text) - Double.Parse(mondayValMaskedTextBox.Text)
+            secondDayChange = Double.Parse(wednesdayValMaskedTextBox.Text) - Double.Parse(tuesdayValMaskedTextBox.Text)
+            'HoldBuySell(firstDayChange, secondDayChange)
+            decision = CStr(HoldBuySell(firstDayChange, secondDayChange))
+            'Console.WriteLine(firstDayChange)
+            'Console.WriteLine(secondDayChange)
+            'Console.WriteLine(decision)
+            Console.WriteLine(ColumnHeaders(stockCodeTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+            Select Case decision
+                Case "Buy"
+                    summary.Insert(0, ColumnHeaders(stockCodeTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    stockListBox.Items.Insert(0, ColumnHeaders(stockCodeTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    buyCount += 1
+
+
+                Case "Sell"
+                    summary.Insert(buyCount, ColumnHeaders(stockCodeTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    stockListBox.Items.Insert(buyCount, ColumnHeaders(stockCodeTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    sellCount += 1
+                Case "Hold"
+                    endSummary.Add(ColumnHeaders(stockCodeTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    stockListBox.Items.Insert((buyCount + sellCount), ColumnHeaders(stockCodeTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+            End Select
+            Console.WriteLine(summary)
+            'clearData()
+
+
+        End If
+
+    End Sub
+    Private Function HoldBuySell(firstDayChange As Double, secondDayChange As Double) As Object
+        Dim result As String
+        If firstDayChange > 0 And secondDayChange > firstDayChange Then
+            result = "Buy"
+        ElseIf firstDayChange < 0 And secondDayChange < firstDayChange Then
+            result = "Sell"
+        Else
+            result = "Hold"
+        End If
+
+
+
+        Return result
+
+    End Function
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click,
+        ClearToolStripMenuItem1.Click
+        clearData()
+
+    End Sub
+
+    Private Sub SummaryToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SummaryToolStripMenuItem.Click, SummaryToolStripMenuItem1.Click
+
+
+    End Sub
+
+End Class
