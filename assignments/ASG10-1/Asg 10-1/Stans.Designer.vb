74b184e1dcd230678113c187782ecaee8df0e838
carlnathF19 <54322367+carlnathF19@users.noreply.github.com>
2019-12-04 18:54:16 -0700

    Assignment 10-1 uploaded
    Asg 10-1 has a Grocery store array that demonstrates knowledge of how to search through multi-dimension arrays, and read info in from a file.

---
+++ assignments/ASG10-1/Asg 10-1/Stans.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StansGrocery
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.inventoryListBox = New System.Windows.Forms.ListBox()
+        Me.InventoryComboBox = New System.Windows.Forms.ComboBox()
+        Me.lookupTextBox = New System.Windows.Forms.TextBox()
+        Me.TextboxInventorySearchLabel = New System.Windows.Forms.Label()
+        Me.searchTextBoxButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(560, 380)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(228, 58)
+        Me.exitButton.TabIndex = 0
+        Me.exitButton.Text = "&Exit"
+        Me.ToolTip1.SetToolTip(Me.exitButton, "Press Alt+X or click here to exit")
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'inventoryListBox
+        '
+        Me.inventoryListBox.FormattingEnabled = True
+        Me.inventoryListBox.ItemHeight = 16
+        Me.inventoryListBox.Location = New System.Drawing.Point(215, 91)
+        Me.inventoryListBox.Name = "inventoryListBox"
+        Me.inventoryListBox.Size = New System.Drawing.Size(583, 260)
+        Me.inventoryListBox.TabIndex = 1
+        '
+        'InventoryComboBox
+        '
+        Me.InventoryComboBox.FormattingEnabled = True
+        Me.InventoryComboBox.Location = New System.Drawing.Point(12, 170)
+        Me.InventoryComboBox.Name = "InventoryComboBox"
+        Me.InventoryComboBox.Size = New System.Drawing.Size(197, 24)
+        Me.InventoryComboBox.TabIndex = 2
+        '
+        'lookupTextBox
+        '
+        Me.lookupTextBox.Location = New System.Drawing.Point(389, 63)
+        Me.lookupTextBox.Name = "lookupTextBox"
+        Me.lookupTextBox.Size = New System.Drawing.Size(197, 22)
+        Me.lookupTextBox.TabIndex = 3
+        '
+        'TextboxInventorySearchLabel
+        '
+        Me.TextboxInventorySearchLabel.Location = New System.Drawing.Point(12, 91)
+        Me.TextboxInventorySearchLabel.Name = "TextboxInventorySearchLabel"
+        Me.TextboxInventorySearchLabel.Size = New System.Drawing.Size(197, 76)
+        Me.TextboxInventorySearchLabel.TabIndex = 4
+        Me.TextboxInventorySearchLabel.Text = "Groceries can also be selected in the drop down box, or the list box to the right" &
+    "."
+        '
+        'searchTextBoxButton
+        '
+        Me.searchTextBoxButton.Location = New System.Drawing.Point(592, 63)
+        Me.searchTextBoxButton.Name = "searchTextBoxButton"
+        Me.searchTextBoxButton.Size = New System.Drawing.Size(75, 23)
+        Me.searchTextBoxButton.TabIndex = 5
+        Me.searchTextBoxButton.Text = "&Search"
+        Me.searchTextBoxButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(212, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(171, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "What are you looking for?"
+        '
+        'StansGrocery
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.searchTextBoxButton)
+        Me.Controls.Add(Me.TextboxInventorySearchLabel)
+        Me.Controls.Add(Me.lookupTextBox)
+        Me.Controls.Add(Me.InventoryComboBox)
+        Me.Controls.Add(Me.inventoryListBox)
+        Me.Controls.Add(Me.exitButton)
+        Me.Name = "StansGrocery"
+        Me.Text = "Stans Grocery"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents exitButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents inventoryListBox As ListBox
+    Friend WithEvents InventoryComboBox As ComboBox
+    Friend WithEvents lookupTextBox As TextBox
+    Friend WithEvents TextboxInventorySearchLabel As Label
+    Friend WithEvents searchTextBoxButton As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ assignments/ASG10-1/Asg 10-1/Stans.vb
@@ -0,0 +1,119 @@
+﻿'Nathan Carlson
+'RCET0265
+'asg 10-1   
+'Stans
+'https://github.com/carlnathF19/NDC-VS-F19/tree/master/assignments/Asg8-1
+
+'Option Explicit On
+'Option Strict On is turned off because it disallows late binding, which would require declaration of variables to store current selected items
+'it is more efficient to allow the late binding and only turn Option Strict On when troubleshooting code.
+
+
+Public Class StansGrocery
+    'Dimensions a 3 X 5 Aisle, Item array to store inventory
+    Dim Food$(2, 4)
+    Private Sub ReadFile(ByVal fileName As String, ByRef recordData() As String)
+        'Reads in a file with the first argument being it's location, and the second an array.  The  functions FileOpen, and Input use the filename to 
+        'identify the file location, and filenumber, which is set by freefile, to identify the file. 
+        Dim currentRecord As String
+        Dim fileData As String
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            'Attempts to read in the file, looping through each line until it reaches the End of File, EOF.  Filedata is read into currentRecord
+            FileOpen(fileNumber, fileName, OpenMode.Input)
+            Do While Not EOF(fileNumber)
+                Input(fileNumber, currentRecord)
+                fileData &= currentRecord
+            Loop
+        Catch ex As Exception
+            'If the file does not exist displays an error message.
+            Console.WriteLine(ex.Message)
+        Finally
+            'Closes the file, necessary to keep memory free.  If fileNumber is not specified closes all files.  If Append or Output was the openmode, 
+            'the remaining data is written to the file.
+            FileClose(fileNumber)
+        End Try
+        'using the split command to record information into the recordData array, splitting into a new item at each $$.
+        recordData = Split(fileData, "$$")
+
+    End Sub
+
+    Private Sub StansGrocery_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'On load reads in the file with ReadFile Sub.  
+        Dim originalFileName As String = "C:\Users\carri\Documents\GitHub\NDC-VS-F19\assignments\ASG10-1\Asg 10-1\grocery.txt"
+        Dim inventoryIn() As String
+        'The first read in information is an empty string, so by Dimensioning i at 1 the first blank is always skipped, and also resets with each load.
+        Dim i As Integer = 1
+
+        ReadFile(originalFileName, inventoryIn)
+
+        For aisle = 0 To 2
+            For item = 0 To 4
+
+                Food(aisle, item) = inventoryIn(i)
+                InventoryComboBox.Items.Add(inventoryIn(i))
+                inventoryListBox.Items.Add(inventoryIn(i))
+                i += 1
+            Next
+        Next
+
+
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub inventoryListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles inventoryListBox.SelectedIndexChanged
+        'Loops through each array position and checks to see if the item is equal to the Listbox selected item.  In order to check that
+        'both values are equal, tolower is used to compare each in it's lower case state.  More complex coding could identify if a plural is used.
+        'Each check will use tolower.  Then this sub shows item aisle and number, by adding one to it's array index row and column.  
+        'If it is found it displays a message with the location.
+        For aisle = 0 To 2
+            For item = 0 To 4
+                If Food(aisle, item).ToLower = inventoryListBox.SelectedItem.tolower Then
+                    MessageBox.Show("You will find " & inventoryListBox.SelectedItem & "s on aisle #" & aisle + 1 & ", item # " & item + 1)
+                End If
+            Next
+        Next
+
+
+    End Sub
+
+    Private Sub InventoryComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles InventoryComboBox.SelectedIndexChanged
+        'Loops through each array position and checks to see if the item is equal to the Combo box selected item.  Then shows its aisle and number,
+        'by adding one to it's array index row and column.  If it is found it displays a message with the location.
+        For aisle = 0 To 2
+            For item = 0 To 4
+                If Food(aisle, item).ToLower = InventoryComboBox.SelectedItem.tolower Then
+                    MessageBox.Show("You will find " & Food(aisle, item) & "s on aisle #" & aisle + 1 & ", item # " & item + 1)
+                End If
+            Next
+        Next
+        'Console.WriteLine(InventoryComboBox.SelectedItem)
+    End Sub
+
+    Private Sub searchTextBoxButton_Click(sender As Object, e As EventArgs) Handles searchTextBoxButton.Click
+        'Initiaties a search, and sets isFound to it's default "Item not found" message.
+        Dim isFound As String = "The item you are searching for is not in the store"
+        'Checks to see if search is zzz, then closes if true, else ignores.
+        If lookupTextBox.Text = "zzz" Then
+            Me.Close()
+        End If
+
+        'Loops through each array position and checks to see if the item is equal to the read in text.  
+        'Then shows its aisle and number, by adding one to it's array index row and column.  If it is found it displays a message with the location.
+        For aisle = 0 To 2
+            For Item = 0 To 4
+                If Food(aisle, Item).ToLower = lookupTextBox.Text.ToLower Then
+                    'updates isFound if the item is found.
+                    isFound = "You will find " & Food(aisle, Item) & "s on aisle #" & aisle + 1 & ", item #" & Item + 1
+                End If
+            Next
+        Next
+        'Displays the location of the item, or the "Item not found message.
+        MessageBox.Show(isFound)
+
+    End Sub
+End Class
